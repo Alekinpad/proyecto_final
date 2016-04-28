aeb90fae44e17ce8062c214db7316b8fa9cd7d0a
Francisca Medina <fbmedina@uc.cl>
2016-04-28 19:44:21 -0300

    se le agregaron las variables a devise.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,8 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     # Attempt to find the User
     @user = User.find_for_facebook_oauth(
-    request.env["omniauth.auth"] )
+    request.env["omniauth.auth"] 
+    )
     if @user.persisted?
       flash[:notice] = "Has ingresado via Facebook" 
       sign_in_and_redirect @user, :event => :authentication
