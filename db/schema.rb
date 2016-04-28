017b7166f56942dda4b2a82eadcc8b6d2b3bebe7
Francisca Medina <fbmedina@uc.cl>
2016-04-28 20:14:12 -0300

    Se le agregó el género al user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160428175111) do
+ActiveRecord::Schema.define(version: 20160428231307) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160428175111) do
     t.integer  "age"
     t.string   "phone_number"
     t.string   "avatar"
+    t.integer  "gender"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
