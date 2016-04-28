28d63a915879f9c99e796734a5d5ae59d7034d3f
Francisca Medina <fbmedina@uc.cl>
2016-04-27 22:03:15 -0300

    tratando de arreglar el error The parameter app_id is required

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,4 +1,16 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  def facebook
+    # Attempt to find the User
+    @user = User.find_for_facebook_oauth(
+    request.env["omniauth.auth"] )
+    if @user.persisted?
+      flash[:notice] = "Has ingresado via Facebook" 
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      redirect_to new_user_registration_url
+    end 
+  end
+
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
   
