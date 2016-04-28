b74eb22d551beda99469dcafaa6d32537c478243
Francisca Medina <fbmedina@uc.cl>
2016-04-27 21:43:03 -0300

    se genraron las vistas de devise

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
-
+  
   # You should also create an action method in this controller like this:
   # def twitter
   # end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   devise :omniauthable, :omniauth_providers => [:facebook]
+
+  def self.find_for_facebook_oauth(auth)
+    user = User.where(provider: auth.provider, uid: auth.uid).first # The User was found in our database
+    return user if user
+    
+    # Check if the User is already registered without Facebook
+    user = User.where(email: auth.info.email).first return user if user
+
+    User.create(
+      name: auth.extra.raw_info.name, provider: auth.provider, uid: auth.uid, email: auth.info.email,
+      password: Devise.friendly_token[0,20])
+  end
 end
