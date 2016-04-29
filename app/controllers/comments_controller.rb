fb8f15549dbc0d02fdc80fd9754851769b0c02d4
Francisca Medina <fbmedina@uc.cl>
2016-04-29 18:06:52 -0300

    se agregó a los show de store y product un render partial que mostrara los comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,15 @@
+class CommentController < ApplicationController
+  before_action :authenticate_user!
+
+  def create
+    @comment = @commentable.comments.new comment_params
+    @comment.user = current_user
+    @comment.save
+    redirect_to @commentable, notice: "Comentario enviado"
+  end
+
+  private
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
+end
