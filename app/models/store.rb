9bcab71b6db819e334b63f0663ab0b25f8df5571
Francisca Medina <fbmedina@uc.cl>
2016-05-02 00:13:11 -0300

    Lo anterior era a nivel de modelo con validates uniqueness... Ahora al borrarse una tienda se bpran con ella todos los productos

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,6 @@
 class Store < ActiveRecord::Base
   belongs_to :user
-  has_many :products
+  has_many :products, dependent: :destroy
   has_many :comments, as: :commentable
   has_many :likes, as: :likeable
 
