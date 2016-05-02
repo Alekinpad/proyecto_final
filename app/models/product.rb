7c4fb5d965efafedf19f0d835c7dff35f4e3e11e
Francisca Medina <fbmedina@uc.cl>
2016-05-02 20:08:51 -0300

    Se hizo el diagrama via railroady

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,6 @@ class Product < ActiveRecord::Base
   has_many :comments, as: :commentable, dependent: :destroy
   has_many :likes, as: :likeable, dependent: :destroy
   has_many :user_likes, through: :likes, source: :user
-  has_many :product_tags
-  has_many :tags, through: :product_tags
 
   def to_s
     self.name
