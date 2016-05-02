ad1e8a60ec9cdfcc8bcd80ae7896f056d6bca874
Francisca Medina <fbmedina@uc.cl>
2016-05-02 16:53:35 -0300

    Ahora están bien hechas las relaciones... User has many comments y likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_one :store
+  has_many :likes
+  has_many :comments
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
