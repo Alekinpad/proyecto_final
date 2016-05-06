a7c9e3fc1687c090e330db1686bac2afc4b49d16
Francisca Medina <fbmedina@uc.cl>
2016-05-06 18:02:52 -0300

    Se solucionó el problema del archivo seeds, sólo se pueden crear tiendas si hay un usuario asociado a ellas

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,21 +10,37 @@ Product.destroy_all
 Store.destroy_all
 User.destroy_all
 
+users = []
 stores = []
 
+5.times do |u|
+  users << User.create!(
+    name: Faker::Name.name,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    email: Faker::Internet.email,
+    password: "password",
+    role: 1,
+    gender: "female"
+  )
+end
+
 5.times do |s|
-  stores << Store.create(
+  u = users[s]
+  stores << Store.create!(
     name: Faker::Company.name,
-    description: Faker::Lorem.sentence(5)
+    description: Faker::Lorem.sentence(5),
+    user: u
   )
 end
 
 20.times do |p|
   Product.create(
-      name: Faker::Commerce.product_name,
-      description: Faker::Lorem.sentence(3),
-      price: Faker::Number.between(3000, 10000),
-      stock: Faker::Number.between(1, 10),
-      store: stores.sample
-    )
+    name: Faker::Commerce.product_name,
+    description: Faker::Lorem.sentence(3),
+    price: Faker::Number.between(3000, 10000),
+    stock: Faker::Number.between(1, 10),
+    store: stores.sample
+  )
 end
+
+
