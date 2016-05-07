8623ac7c2485fa080c4d0df74df114b16a7c3f83
Francisca Medina <fbmedina@uc.cl>
2016-05-07 16:52:25 -0300

    Todavía problemas... Esta vez con like y comment en PRODUCT

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -57,6 +57,7 @@ class ProductsController < ApplicationController
   # DELETE /products/1
   # DELETE /products/1.json
   def destroy
+    @store = Store.find(params[:store_id])
     @product.destroy
     respond_to do |format|
       format.html { redirect_to products_url, notice: 'Product was successfully destroyed.' }
